6e1133ed39262fd2111092245431beb64092c1a2
Eglecio A. Pereira <eglecio@hotmail.com>
2025-01-25 15:06:15 -0300

    ADD EF8 ao projeto.\n ADD projeto de classes 'DOMINIO' onde centralizamos as entidades de persistencia ao banco Implementado Interface e classes de REPOSITORIO para CRUD. Add classe de entidade base para as demais no sistema, a qual tera um ID e um campo EXCLUIDO que serao base em todo o sistema, visto que NAO SERA DELETADO nenhum registro, apenas atualizado como EXCLUIDO, a fim de mantermos historico

---
+++ Dominio/Interfaces/IRepositorio.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Dominio.Interfaces {
+
+  public interface IRepositorio<T> : IDisposable where T : class {
+    Task<T> AdicionarAsync(T entidade);
+    Task AtualizarAsync(T entidade);
+    Task RemoverAsync(T entidade);
+    Task RemoverPorIdAsync(object id);
+
+    // Métodos de Busca...
+    Task<T> ObterPorIdAsync(object id);
+    Task<IEnumerable<T>> BuscarAsync(Expression<Func<T, bool>> filtro);
+    Task<IEnumerable<T>> BuscarPaginadoAsync(
+        Expression<Func<T, bool>> filtro,
+        int pagina,
+        int itensPorPagina,
+        Expression<Func<T, object>>? ordenacao = null
+    );
+
+  }
+}
