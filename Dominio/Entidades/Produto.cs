df6c923bb1573aa7b6ab0ec06f610bf362b8caea
Eglecio A. Pereira <eglecio@hotmail.com>
2025-01-25 15:50:24 -0300

    ADD entidades de dominio que serao usadas no projeto.

---
+++ Dominio/Entidades/Produto.cs
@@ -0,0 +1,12 @@
+﻿using Dominio.Enumeradores;
+
+namespace Dominio.Entidades {
+  public class Produto : EntidadeBase {
+    public string Nome { get; set; }
+    public string Observacao { get; set; }
+    public double QuantidadeEstoque { get; set; }
+    public double ValorCusto { get; set; }
+    public double ValorVenda { get; set; }
+    public DateTime DataCadastro { get; set; } = DateTime.Today;
+  }
+}
---
+++ Dominio/Entidades/Usuario.cs
@@ -0,0 +1,11 @@
+﻿using Dominio.Enumeradores;
+
+namespace Dominio.Entidades {
+  public class Usuario : EntidadeBase {
+    public string Email { get; set; }
+    public string Senha { get; set; }
+    public PerfilUsuario Perfil { get; set; } = PerfilUsuario.Padrao;
+    public DateTime DataCadastro { get; set; } = DateTime.Today;
+    public DateTime DataInativacao { get; set; } = DateTime.MaxValue;
+  }
+}
