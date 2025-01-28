dc9d3adc588f60f1204977d502d12b89afef70e6
Eglecio A. Pereira <eglecio@hotmail.com>
2025-01-28 00:45:19 -0300

    Feature remocao de cliente no APP

--- Dominio/obj/Debug/net8.0/Dominio.AssemblyInfo.cs
+++ Dominio/obj/Debug/net8.0/Dominio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dominio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a09d17d609f3ed67603517be6752ebf774f59b5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f399ec756aae1785ad4c754073bd6c97e08c2c7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dominio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dominio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
